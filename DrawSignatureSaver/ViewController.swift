b787dd512f8418a14e918c1d83878742a837f3dc
FTS-MAC-014 <fingent@ftsmac014smini.ftsindia.in>
2016-12-08 17:36:49 +0530

    initial

--- DrawSignatureSaver/ViewController.swift
+++ DrawSignatureSaver/ViewController.swift
@@ -1,18 +1,50 @@
 //
 //  ViewController.swift
 //  DrawSignatureSaver
-//
-//  Created by FTS-MAC-014 on 08/12/16.
-//  Copyright © 2016 FTS-MAC-014. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, DrawSignatureDelegate {
 
+    @IBOutlet weak var sampleSignatureView: DrawSignatureView!
+    
+    @IBOutlet weak var savedSignatureImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sampleSignatureView.delegate = self
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        sampleSignatureView.backgroundColor =  UIColor.lightGray
+    }
+    @IBAction func clearSignature(_ sender: Any) {
+        
+        sampleSignatureView.clear()
+        savedSignatureImage.image = nil
+        
+        
+    }
+    @IBAction func saveSignatureImage(_ sender: Any) {
+        
+        if sampleSignatureView.doesContainSignature {
+            let savedImage = sampleSignatureView.getCroppedSignature()
+            // Do something with img
+            savedSignatureImage.image =  savedImage
+            self.viewWillAppear(true)
+        } else {
+     }
+    }
+    
+    // Optional delegate methods
+    func startedDrawing() {
+        // Do something when start drawing
+    }
+    
+    func finishedDrawing() {
+        // Do something else when finished drawing
     }
 
     override func didReceiveMemoryWarning() {
